411042e6d6e634d8882effb258e8e24468ff4085
JiminIT <yhhs7741@naver.com>
2019-11-28 14:59:47 +0900

    MeowMeowPunch Project

--- app/src/main/java/kr/hs/emirim/meowmeowpunch/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/meowmeowpunch/MainActivity.java
@@ -3,7 +3,10 @@ package kr.hs.emirim.meowmeowpunch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.m_stBtn).setOnClickListener(m_stBtnClick);
+        findViewById(R.id.m_rnkBtn).setOnClickListener(m_rnkBtnClick);
     }
+
+    Button.OnClickListener m_stBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, StartActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    Button.OnClickListener m_rnkBtnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, RankActivity.class);
+            startActivity(intent);
+        }
+    };
 }
---
+++ app/src/main/java/kr/hs/emirim/meowmeowpunch/RankActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.meowmeowpunch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rank);
+    }
+}
